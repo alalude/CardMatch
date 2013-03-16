baa45c38ac4129a5a7c478ea39c94db7435f07f0
Adesina <adesina.lalude@gmail.com>
2013-03-15 20:42:45 -0400

    Added branch name to controller.h (SingleCard)

--- CardMatch/CardGameViewController.h
+++ CardMatch/CardGameViewController.h
@@ -6,6 +6,12 @@
 //  Copyright (c) 2013 Akinbiyi Lalude. All rights reserved.
 //
 
+//
+// This is a branch
+// Local Name = SingleCard
+// Remote Name = singlecard
+//
+
 #import <UIKit/UIKit.h>
 
 @interface CardGameViewController : UIViewController
